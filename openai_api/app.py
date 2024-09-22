639cc0ed0a121a8c8638bbc8b14651926b365025
Pragnesh Anekal <72695068+Pragnesh260@users.noreply.github.com>
2024-09-22 01:05:51 -0400

    Test UI Revamp

--- openai_api/app.py
+++ openai_api/app.py
@@ -5,11 +5,10 @@ import streamlit as st
 from data.data_read import fetch_data_from_db,fetch_data_from_db_dashboards
 from openai_api.openai_api_call import OpenAIClient
 from streamlit_ui.streamlit_custom_css import apply_custom_css
-from streamlit_ui.streamlit_home_page import render_home_page
+from pages.streamlit_home_page import render_home_page
 from streamlit_ui.streamlit_dashboard_page import render_dashboard_page
-from streamlit_ui.streamlit_predicting_page import render_predicting_page
-from streamlit_ui.streamlit_predicting_page import reset_interaction_state
-
+from pages.streamlit_predicting_page import render_predicting_page
+from pages.streamlit_predicting_page import reset_interaction_state
 
 def setup_session_state():
     """Initialize session state variables."""
@@ -33,23 +32,23 @@ def setup_session_state():
         st.session_state.steps_text = ""
 
 
-def render_sidebar():
-    """Render the sidebar with navigation buttons."""
-    st.sidebar.title("Navigation")
-    if st.sidebar.button("Home"):
-        st.session_state.page = "Home"
-        reset_interaction_state()  # Reset the interaction state when navigating to the Home page
-    if st.sidebar.button("Predicting"):
-        st.session_state.page = "Predicting"
-        reset_interaction_state()  # Reset the interaction state when navigating to the Predicting page
-    if st.sidebar.button("Dashboard"):
-        st.session_state.page = "Dashboard"
-        reset_interaction_state()  # Reset the interaction state when navigating to the Dashboard page
+# def render_sidebar():
+#     """Render the sidebar with navigation buttons."""
+#     st.sidebar.title("Navigation")
+#     if st.sidebar.button("Home"):
+#         st.session_state.page = "Home"
+#         reset_interaction_state()  # Reset the interaction state when navigating to the Home page
+#     if st.sidebar.button("Predicting"):
+#         st.session_state.page = "Predicting"
+#         reset_interaction_state()  # Reset the interaction state when navigating to the Predicting page
+#     if st.sidebar.button("Dashboard"):
+#         st.session_state.page = "Dashboard"
+#         reset_interaction_state()  # Reset the interaction state when navigating to the Dashboard page
 
 def main():
     
     """Main function to control the flow of the app."""
-    apply_custom_css()
+    # apply_custom_css()
     setup_session_state()
 
     openai_client = OpenAIClient()
@@ -57,7 +56,7 @@ def main():
     # Call the function and store the result in a DataFrame
     data_frame = fetch_data_from_db()
     data_frame_dashboard=fetch_data_from_db_dashboards()
-    render_sidebar()
+    # render_sidebar()
 
     if st.session_state.page == "Home":
         render_home_page()
---
+++ pages/1_predicting_page.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from data.data_read import insert_model_response
+from datetime import datetime
+from data.data_s3 import download_file, process_data_and_generate_url, RETRIEVAL_EXT, CI_EXT, IMG_EXT, MP3_EXT
+import json
+from data.data_read import fetch_data_from_db, fetch_data_from_db_dashboards
+
+# Handle file processing and download
+def handle_file_processing(question_selected):
+    """Process and download the associated file for the selected question."""
+    file_name = process_data_and_generate_url(question_selected)
+    if file_name == 1:
+        st.write('No file is associated with this question')
+        return None
+    else:
+        loaded_file = download_file(file_name)
+        st.write('Download file:', file_name)
+        return loaded_file
+
+# Initialize session state for the data
+if 'data_frame' not in st.session_state:
+    st.session_state.data_frame = fetch_data_from_db()
+if 'db_data_frame' not in st.session_state:
+     st.session_state.db_data_frame = fetch_data_from_db_dashboards()
+
+question_selected = st.selectbox(
+        "**Select a Question:**", 
+        options=list(st.session_state.data_frame['Question']),
+        index=None,
+        key="question_selector",
+    )
+
+if question_selected:
+        st.text_area("**Selected Question**:", question_selected)
+        validate_answer = st.session_state.data_frame[st.session_state.data_frame['Question'] == question_selected]
+        task_id_sel = validate_answer['task_id'].iloc[0]
+        validate_answer = validate_answer['Final answer'].iloc[0]
+        st.write('Selected Question Answer is:', validate_answer)
+
+        print(type(question_selected))
+
+        loaded_file = handle_file_processing(question_selected)
